b2caf762e43cdac92fafa8e7c88e9419165a373d
CoroSempi <corosempi@gmail.com>
2024-05-02 21:53:38 +0300

    Changeing path to link

--- index.js
+++ index.js
@@ -36,25 +36,9 @@ app.use("/profile", profile);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
-const corsOptions = {
-  origin: "*",
-  credentials: true,
-  optionSuccessStatus: 200,
-};
 
-app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
+
+app.use(cors({origin:"*",}));
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
