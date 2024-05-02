672c797a79e8845673c1b1fbe092a638081f61b6
CoroSempi <corosempi@gmail.com>
2024-05-02 20:44:37 +0300

    Changeing path to link

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ app.use("/profile", profile);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
+app.use(cors)
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
