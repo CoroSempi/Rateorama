9f51395bba81b21886d83d98dabb376bd791d0fc
CoroSempi <corosempi@gmail.com>
2024-05-02 21:03:17 +0300

    Changeing path to link

--- index.js
+++ index.js
@@ -45,8 +45,14 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   next();
 });
 app.set("views", __dirname + "/views");
