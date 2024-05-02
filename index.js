660ee152e7cab2c7fe4c668c8b37e763cc87d2ca
CoroSempi <corosempi@gmail.com>
2024-05-02 21:10:58 +0300

    Changeing path to link

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const path = require("path");
 const cors = require("cors");
 
-// Import route handlers
 const dashboard = require("./Conroller/dashboard");
 const users = require("./Conroller/users");
 const home = require("./Conroller/home");
@@ -11,17 +12,17 @@ const seriesRoute = require("./Conroller/series");
 const tvshowsRoute = require("./Conroller/tvshows");
 const userlists = require("./Conroller/userlists");
 const profile = require("./Conroller/profile");
+// Rateorama Schemes(models)
+const Users = require("./Models/Users");
+const Mails = require("./Models/Mails");
+const Comments = require("./Models/Comments");
+const RateBase = require("./Models/RateBase");
+const UserLists = require("./Models/UsersLists");
 
+// Create a Server
 const app = express();
 
-// Middleware
-app.use(cors()); // Enable CORS for all routes
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public"));
-
-// Routes
+//routes
 app.use("/dashboard", dashboard);
 app.use("/users", users);
 app.use("/home", home);
@@ -31,34 +32,56 @@ app.use("/tvshows", tvshowsRoute);
 app.use("/userlists", userlists);
 app.use("/profile", profile);
 
-// Set view engine
+// Middleware for parsing JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
-const port = process.env.PORT || 3000;
+let port = 3000 || process.env.PORT;
 
-run().then(() => {
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+run();
+app.listen(port, () => {
+  console.log(`SERVER RUN ON PORT ${port}`);
 });
 
 async function run() {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://<username>:<password>@<cluster>/<database>?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Failed to connect to MongoDB:", error);
-    process.exit(1); // Exit the process if unable to connect to MongoDB
-  }
+  await mongoose
+    .connect(
+      "mongodb+srv://seif:Liverpool6@seif.54v3nl4.mongodb.net/?retryWrites=true&w=majority&appName=seif"
+    )
+    .then(console.log("hh"));
 }
 
+app.get("/Admins", (req, res) => {
+  try {
+    res.status(200).render("adminSignin");
+    return;
+  } catch (error) {
+    res.status(400).send("Sorry there is an ERROR!");
+    console.log("ERROR !:" + error.message);
+  }
+});
 //Connect to MongoDB using mongoose(ODM)
 // mongoose
 //   .connect("mongodb+srv://seif:Liverpool6@seif.54v3nl4.mongodb.net/")
