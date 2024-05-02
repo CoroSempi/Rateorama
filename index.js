09a966f79ae6cee98e8a0a4bf9ce2abb8e308e3e
CoroSempi <corosempi@gmail.com>
2024-05-02 20:22:19 +0300

    Changeing path to link

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const path = require("path");
+const cors = require("cors");
+
 
 const dashboard = require("./Conroller/dashboard");
 const users = require("./Conroller/users");
@@ -35,6 +37,7 @@ app.use("/profile", profile);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
+app.use(cors());
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
@@ -78,163 +81,36 @@ app.get("/", (req, res) => {
   res.status(200).send("finally hellooooo");
 });
 
-// async function use() {
-//   try {
-//     const hashed = await bcrypt.hash("1234", 10);
-//     console.log(hashed);
-//     const mm = await Admins.create({
-//       userName: "abdo",
-//       password: hashed,
-//     });
-
-//     console.log(mm);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-// use();
-
-// ahmed
-// alioo
-// momo
-
-// async function use() {
-//   try {
-//     const mm = await Users.create({
-//       userName: "alii",
-//       email: "DZAF@YAHOO.COM",
-//       password: "hello seif",
-//     });
-//     console.log(mm);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-// use();
-// async function mailll() {
-//   try {
-//     const mm = await Mails.create({
-//       userName: "alii",
-//       content: "hello momo",
-//     });
-//     console.log(mm)
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-// mailll()
-// async function mailll() {
-//   try {
-//     const mail = {
-//       content: "fsefgfergergre",
-//     };
-
-//     const cc = await Mails.findOneAndUpdate(
-//       { userName: "seif" },
-//       { $push: { usermails: mail } },
-//       { new: true }
-//     );
-
-//     console.log(cc);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-
-// mailll()
-// async function run() {
-//   try {
-//     const cc = await Comments.create({
-//       moviesID: 12,
-//       movieComments: [
-//         {
-//           userName: "seif",
-//           content: "gfergre",
-//         },
-//       ],
-//     });
-//     console.log(cc);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-
-// async function run() {
-//   try {
-//     const comment = {
-//       userName: "ali",
-//       content: "erehrthtrherhetrh6ujej",
-//     };
-
-//     const cc = await Comments.findOneAndUpdate(
-//       { moviesID: 12 },
-//       { $push: { movieComments: comment } },
-//       { new: true }
-//     );
-
-//     // console.log(cc);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-
-// run();
-
-// run();
-// async function run2() {
-//   ss = await Comments.findOne({moviesID:12});
-//   console.log(ss)
-// }
-
-// run2()
-// async function run() {
-//   try {
-//     const updatedComment = {
-//       userName: "ahmed",
-//       commentID: "1",
-//       content: "Updated content",
-//     };
-
-//     const cc = await Comments.findOneAndUpdate(
-//       { moviesID: 12, "movieComments.userName": "ahmed" },
-//       { $set: { "movieComments.$": updatedComment } },
-//       { new: true }
-//     );
-
-//     console.log(cc);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-
-// async function run() {
-//   try {
-//     const b = await Movies.exists({ name: "Shutter Island" });
-//     if (b === null) {
-//       const a = await Movies.create({
-//         name: "Shutter Island",
-//         year: 2010,
-//         RelaseDate: "Feb 01, 2024",
-//         story:
-//           "Teddy Daniels and Chuck Aule, two US marshals, are sent to an asylum on a remote island in order to investigate the disappearance of a patient, where Teddy uncovers a shocking truth about the place.",
-//         Rate: 4.1,
-//         poster: "./pubic/moviespics/shuuterIslandPoseter",
-//         bgPic: "./pubic/moviespics/shuuterIslandBg",
-//         trailerLink: "https://www.youtube.com/watch?v=YDGldPitxic",
-//         genres: ["action", "hrror"],
-//         forAdults: true,
-//       });
-
-//       console.log(a);
-//       await a.save();
+// fetch("https://rateorama.vercel.app/HOME/search?keyword=dark")
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Network response was not ok");
 //     }
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-// run();
-// async function m() {
-//   const wwe = await Movies.find();
-//   console.log(wwe);
-// }
-// m();
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     console.log(
+//       "========================================================================"
+//     );
+//   })
+//   .catch((error) => {
+//     console.error("There was a problem with the fetch operation:", error);
+//   });
+
+fetch("https://rateorama.vercel.app/home/toprated")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    console.log(
+      "========================================================================"
+    );
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
